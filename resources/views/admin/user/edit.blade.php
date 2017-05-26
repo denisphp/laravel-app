a565e3ea92852680994e6509ac4486497f47cee7
nabatov <d.nabatov@mobidev.biz>
2017-05-26 15:34:25 +0300

    update admin User CRUD

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -4,18 +4,23 @@
     <div id="edit">
         <h1>Edit User# {{ $user->id }}</h1>
         <!-- if there are creation errors, they will show here -->
-        {{ HTML::ul($errors->all()) }}
 
         {{ Form::model($user, ['route' => ['admin.users.update', $user->id], 'method' => 'POST']) }}
 
         <div class="form-group">
             {{ Form::label('name', 'Name') }}
             {{ Form::text('name', null, ['class' => 'form-control']) }}
+            @if ($errors->has('name'))
+                <span class="error">{{ $errors->first('name') }}</span>
+            @endif
         </div>
 
         <div class="form-group">
             {{ Form::label('email', 'Email') }}
             {{ Form::email('email', null, ['class' => 'form-control']) }}
+            @if ($errors->has('email'))
+                <span class="error">{{ $errors->first('email') }}</span>
+            @endif
         </div>
 
         {{ Form::submit('Update', ['class' => 'btn btn-primary']) }}
