e480ac202a5fbd92c7b255f0110ca4b3bacce774
nabatov <d.nabatov@mobidev.biz>
2017-05-26 18:20:23 +0300

    refactor JWTAuthProvider

--- app/Services/JWTAuthProvider.php
+++ app/Services/JWTAuthProvider.php
@@ -74,9 +74,6 @@ class JWTAuthProvider implements UserProvider
 
     public function byCredentials($credentials = [])
     {
-        // First we will add each credential element to the query as a where clause.
-        // Then we can execute the query and, if we found a user, return it in a
-        // generic "user" object that will be utilized by the Guard instances.
         $query = DB::table('users');
         foreach ($credentials as $key => $value) {
             if (! Str::contains($key, 'password')) {
@@ -84,9 +81,6 @@ class JWTAuthProvider implements UserProvider
             }
         }
 
-        // Now we are ready to execute the query to see if we have an user matching
-        // the given credentials. If not, we will just return nulls and indicate
-        // that there are no matching users for these given credential arrays.
         $user = $query->first();
 
         return $this->getGenericUser($user);
